09f2c2b2c4131136cb0c23233a41d5c283c04869
binsam008 <binsammohammad06@gmail.com>
2025-01-19 19:02:21 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import ScrollToTop from './components/ScrollToTop';
+import { Analytics } from "@vercel/analytics/react"
 import {
   Home,
   About,
@@ -26,6 +27,7 @@ function App() {
         <Contact />
         <Query />
       </main>
+      <Analytics/>
     </div>
   );
 }
